b4011749ef760159326ef7177d0a579d67627521
zaiky09 <zamilmozamil@outlook.com>
2023-02-09 09:28:44 +0300

    Loops done

--- looping.rb
+++ looping.rb
@@ -1,5 +1,11 @@
 def happy_new_year
   # your code here
+  counter = 10
+  until counter == 0
+    puts counter
+    counter -= 1
+  end
+  puts "Happy New Year!"
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -16,9 +22,15 @@ def fizzbuzz(num)
 end
 
 def fizzbuzz_printer
-  # your code here
+  (1..100).each do |num|
+    puts fizzbuzz(num)
+  end
 end
 
 def reverse_string(str)
-  # your code here
+  reverse_string = ""
+  str.length.times do |i|
+    reverse_string  = str[i] + reverse_string
+  end
+  reverse_string
 end
